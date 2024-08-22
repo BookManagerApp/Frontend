3aefb8fec531bf8387dd5cc49c9a136d14a298fa
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-08-22 18:51:19 +0700

    menambahkan endpoint

---
+++ js/url.js
@@ -0,0 +1,10 @@
+export const endpointGetBooks =
+"http://127.0.0.1:3000/books";
+export const endpointGetBookById =
+"http://127.0.0.1:3000/book/getbyid/:id";
+export const endpointPostBook =
+"http://127.0.0.1:3000/book/post";
+export const endpointUpdateBook =
+"http://127.0.0.1:3000/book/update/:id";
+export const endpointDeleteBook =
+"http://127.0.0.1:3000/book/delete/:id";
