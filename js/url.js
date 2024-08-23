f656c6e13479c18031048db689109561aa95ff88
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-08-23 10:46:54 +0700

    menambahkan endpoint

--- js/url.js
+++ js/url.js
@@ -3,6 +3,19 @@ export const endpointGetBookById = "http://127.0.0.1:3000/book/getbyid/:id";
 export const endpointPostBook = "http://127.0.0.1:3000/book/post";
 export const endpointUpdateBook = "http://127.0.0.1:3000/book/update/:id";
 export const endpointDeleteBook = "http://127.0.0.1:3000/book/delete/:id";
+export const endpointGetGenres = "http://127.0.0.1:3000/genres";
+
+// Endpoint Registrasi dan Login
 export const endpointPostRegister = "http://127.0.0.1:3000/register";
 export const endpointPostLogin = "http://127.0.0.1:3000/login";
-export const endpointGetGenres = "http://127.0.0.1:3000/genres";
+
+// Endpoint Dilindungi
+export const endpointProtectedBooks = "http://127.0.0.1:3000/protected/books";
+export const endpointProtectedGetBookById = "http://127.0.0.1:3000/protected/book/getbyid/:id";
+export const endpointProtectedPostBook = "http://127.0.0.1:3000/protected/book/post";
+export const endpointProtectedUpdateBook = "http://127.0.0.1:3000/protected/book/update/:id";
+export const endpointProtectedDeleteBook = "http://127.0.0.1:3000/protected/book/delete/:id";
+export const endpointSomeProtectedHandler = "http://127.0.0.1:3000/protected/endpoint";
+
+// Endpoint Admin
+export const endpointAdminOnly = "http://127.0.0.1:3000/protected/admin/admin-only-endpoint";
